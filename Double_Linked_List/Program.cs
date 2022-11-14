ed912b2469887842bdd500076d5ee74494819f51
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-11-14 20:07:03 +0700

    Add menu ke-3

--- Double_Linked_List/Program.cs
+++ Double_Linked_List/Program.cs
@@ -154,7 +154,8 @@ namespace Double_Linked_List
                 {
                     Console.WriteLine("\nMenu");
                     Console.WriteLine("1. Add a record to the list");
-                    Console.WriteLine("2. delete a record from the list ");
+                    Console.WriteLine("2. Delete a record from the list ");
+                    Console.WriteLine("3. View all records in the ascending order of roll numbers ");
                 }
             }
         }
